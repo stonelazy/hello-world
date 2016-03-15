df5015165ab18a275aedec34ab4929f2b9991d76
sudharsan-2598 <sudharsankp@gmail.com>
2016-03-15 08:28:01 +0530

    first proj commit

---
+++ proj1/helloworld/src/com/test/HelloWorld.java
@@ -0,0 +1,25 @@
+//$Id$
+/**
+ * 
+ */
+package com.test;
+
+import java.util.logging.Logger;
+
+/**
+ * @author sudharsankp
+ */
+public class HelloWorld
+{
+
+	public static Logger log = Logger.getLogger(HelloWorld.class.getName());
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		log.info("checking whether project addition is going to work");
+		
+	}
+
+}
